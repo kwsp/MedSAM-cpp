6c1c212336ae4a83844101b2bc1860957efe2dbb
tnie <tnie@tuta.io>
2025-05-02 15:55:04 -0500

    sam predictor

--- src/sam.cpp
+++ src/sam.cpp
@@ -2433,7 +2433,8 @@ bool sam_params_parse(int argc, char **argv, sam_params &params) {
   return true;
 }
 
-struct sam_predictor {
+class sam_predictor {
+public:
   explicit sam_predictor(sam_params &params) {
     // load the model
     if (!sam_model_load(params, model)) {
@@ -2467,33 +2468,80 @@ struct sam_predictor {
     }
   }
 
-  void encode_image(const cv::Mat & mat1, int n_threads) {
+  void encode_image(const cv::Mat &mat0, int n_threads) {
+    nx0 = mat0.cols;
+    ny0 = mat0.rows;
+    const auto mat1 = sam_image_preprocess(mat0);
     // Encode image
+
+    state.buf_compute_img_enc.resize(ggml_tensor_overhead() *
+                                         GGML_DEFAULT_GRAPH_SIZE +
+                                     ggml_graph_overhead());
+    state.allocr = ggml_gallocr_new(ggml_backend_cpu_buffer_type());
+
+    struct ggml_cgraph *gf = sam_encode_image(model, state, mat1);
+    if (!gf) {
+      char buf[128];
+      snprintf(buf, 128, "%s: failed to encode image\n", __func__);
+      throw std::runtime_error(buf);
+    }
+
+    ggml_graph_compute_helper(state.work_buffer, gf, n_threads);
+
+    // print_t_f32("embd_img", state.embd_img);
+
+    ggml_gallocr_free(state.allocr);
+    state.allocr = nullptr;
+    state.work_buffer.clear();
+  }
+
+  std::vector<cv::Mat> encode_prompts_and_decode_masks(const sam_prompt &prompt,
+                                                       int n_threads) {
+    char buf[128];
+
+    // Encode prompt and decode mask
     {
-      state.buf_compute_img_enc.resize(ggml_tensor_overhead() *
-                                           GGML_DEFAULT_GRAPH_SIZE +
-                                       ggml_graph_overhead());
+      state.buf_compute_fast.resize(ggml_tensor_overhead() *
+                                        GGML_DEFAULT_GRAPH_SIZE +
+                                    ggml_graph_overhead());
       state.allocr = ggml_gallocr_new(ggml_backend_cpu_buffer_type());
 
-      struct ggml_cgraph *gf = sam_encode_image(model, state, mat1);
+      switch (prompt.prompt_type) {
+      case SAM_PROMPT_TYPE_POINT:
+        snprintf(buf, 128, "Using point prompt: (%f, %f)\n", prompt.pt.x,
+                 prompt.pt.y);
+        break;
+      case SAM_PROMPT_TYPE_BOX:
+        snprintf(buf, 128, "Using box prompt: (%f, %f, %f, %f)\n",
+                 prompt.box.x1, prompt.box.y1, prompt.box.x2, prompt.box.y2);
+        break;
+      }
+      printf(buf);
+
+      struct ggml_cgraph *gf =
+          sam_build_fast_graph(model, state, nx0, ny0, prompt);
       if (!gf) {
- char       buf [128];
-        snprintf(buf, 128, "%s: failed to encode image\n", __func__);
+        snprintf(buf, 128, "%s: failed to build fast graph\n", __func__);
         throw std::runtime_error(buf);
       }
 
       ggml_graph_compute_helper(state.work_buffer, gf, n_threads);
 
-      // print_t_f32("embd_img", state.embd_img);
-
+      // print_t_f32("iou_predictions", state.iou_predictions);
+      // print_t_f32("low_res_masks", state.low_res_masks);
       ggml_gallocr_free(state.allocr);
       state.allocr = nullptr;
-      state.work_buffer.clear();
     }
-  }
 
+    return sam_get_masks(model.hparams, nx0, ny0, state);
+  }
+  ~sam_predictor() { ggml_free(model.ctx); }
+
+private:
   sam_model model;
   sam_state state;
+  int nx0{};
+  int ny0{};
 };
 
 int main(int argc, char **argv) {
@@ -2507,56 +2555,20 @@ int main(int argc, char **argv) {
   }
 
   if (params.seed < 0) {
-    params.seed = time(NULL);
+    params.seed = time(nullptr);
   }
   fprintf(stderr, "%s: seed = %d\n", __func__, params.seed);
 
   // load the image
   const auto mat0 = cv::imread(params.fname_inp, cv::IMREAD_COLOR_RGB);
-  const auto mat1 = sam_image_preprocess(mat0);
 
   sam_predictor predictor(params);
 
-  auto &state = predictor.state;
-  auto &model = predictor.model;
+  predictor.encode_image(mat0, params.n_threads);
 
-  predictor.encode_image(mat1, params.n_threads);
+  const auto masks = predictor.encode_prompts_and_decode_masks(
+      params.prompt, params.n_threads);
 
-  // Encode prompt and decode mask
-  {
-    state.buf_compute_fast.resize(ggml_tensor_overhead() *
-                                      GGML_DEFAULT_GRAPH_SIZE +
-                                  ggml_graph_overhead());
-    state.allocr = ggml_gallocr_new(ggml_backend_cpu_buffer_type());
-
-    switch (params.prompt.prompt_type) {
-    case SAM_PROMPT_TYPE_POINT:
-      fprintf(stderr, "Using point prompt: (%f, %f)\n", params.prompt.pt.x,
-              params.prompt.pt.y);
-      break;
-    case SAM_PROMPT_TYPE_BOX:
-      fprintf(stderr, "Using box prompt: (%f, %f, %f, %f)\n",
-              params.prompt.box.x1, params.prompt.box.y1, params.prompt.box.x2,
-              params.prompt.box.y2);
-      break;
-    }
-
-    struct ggml_cgraph *gf =
-        sam_build_fast_graph(model, state, mat0.cols, mat0.rows, params.prompt);
-    if (!gf) {
-      fprintf(stderr, "%s: failed to build fast graph\n", __func__);
-      return 1;
-    }
-
-    ggml_graph_compute_helper(state.work_buffer, gf, params.n_threads);
-
-    // print_t_f32("iou_predictions", state.iou_predictions);
-    // print_t_f32("low_res_masks", state.low_res_masks);
-    ggml_gallocr_free(state.allocr);
-    state.allocr = NULL;
-  }
-
-  const auto masks = sam_get_masks(model.hparams, mat0.cols, mat0.rows, state);
   for (int i = 0; i < masks.size(); ++i) {
     std::string filename = params.fname_out + std::to_string(i) + ".png";
     cv::imwrite(filename, masks[i]);
@@ -2570,8 +2582,6 @@ int main(int argc, char **argv) {
     fprintf(stderr, "%s:    total time = %8.2f ms\n", __func__,
             (t_main_end_us - t_main_start_us) / 1000.0f);
   }
-
-  ggml_free(model.ctx);
 
   return 0;
 }
